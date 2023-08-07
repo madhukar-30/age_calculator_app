00121127dea2e788fd0c1c2a15614cc3d6506642
master-manas <manasmadhukar30@gmail.com>
2023-08-07 14:40:46 +0530

    added readme

---
+++ README-template.md
@@ -0,0 +1,82 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./ss.png)
+
+
+
+### Links
+
+- Solution URL: [Age calculator solution](https://your-solution-url.com)
+- Live Site URL: [Age calculator ](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript
+
+
+
+### What I learned
+This is my first Javascirpt project  I learned alot of things  like :
+>Blur event 
+>keydown event 
+
+
+
+
+### Continued development
+
+I recently completed my first JavaScript project, and it was quite a learning experience for me. It took me some time and effort to finish the project.Through this project, I learned a lot of new things about JavaScript and gained valuable hands-on experience.
+ I plan to continue working on more projects to become even more comfortable with the language and deepen my understanding of its concepts.
+
+
+### Useful resources
+
+- [ Reference for age calculation logic](https://www.geeksforgeeks.org/program-calculate-age/) - This is help me in calculation of Age
+
+
+## Author
+- Frontend Mentor - [@madhukar-30](https://www.frontendmentor.io/profile/madhukar-30)
+
+
+
--- script.js
+++ script.js
@@ -1,133 +1,117 @@
-const dayInput =document.getElementById("day-input");
-const monthInput =document.getElementById("month-input");
-const yearInput =document.getElementById("year-input");
-const dayOutput =document.querySelector(".days-output");
-const monthOutput =document.querySelector(".month-output");
-const yearOutput=document.querySelector(".year-output");
+// Get references to the input elements and output elements
+const dayInput = document.getElementById("day-input");
+const monthInput = document.getElementById("month-input");
+const yearInput = document.getElementById("year-input");
+const dayOutput = document.querySelector(".days-output");
+const monthOutput = document.querySelector(".month-output");
+const yearOutput = document.querySelector(".year-output");
 
-const allInput =document.querySelectorAll("form input")
-const allLabel =document.querySelectorAll("form label")
+// Get references to the error elements
+const dayError = document.querySelector(".day-error");
+const monthError = document.querySelector(".month-error");
+const yearError = document.querySelector(".year-error");
 
-const dayError =document.querySelector(".day-error");
-const  monthError =document.querySelector(".month-error");
-const yearError =document.querySelector(".year-error");
-const form =document.querySelector("form")
-const btn = document.querySelector(".btn")
+// Get reference to the form and the calculate button
+const form = document.querySelector("form");
+const btn = document.querySelector(".btn");
 
+// Array of invalid characters to prevent users from entering them in the input fields
+const invalidChars = ["-", "+", "e"];
 
-const validDayError ="Must be a valid day"
-const EmptyError="This field is required";
-const validMonthError="Must be a valid month"
-const  validYearError="Must be a valid year"
-const futureYearError ="Must be in the past"
+// Array to store the number of days in each month
+let monthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-const invalidChars = [
-   "-",
-   "+",
-   "e",
- ];
- let monthDay =[31, 28, 31, 30, 31, 30, 31, 
-   31, 30, 31, 30, 31 ]
-
- //function to prevent special characters
-form.addEventListener("keydown",function(e){
-   if(invalidChars.includes(e.key)){
-      e.preventDefault();
-   }
-})
-//function to limit the lenght the value of input
-function limitLength(event,value,limit){
-   if(value.toString().length>=limit){
-      event.preventDefault();
-   }
-}
-
-
-
-function checkday(){
-const day =dayInput.value;
-const label =dayInput.previousElementSibling.previousElementSibling;
- dayInput.style.borderColor="hsl(0, 100%, 67%)";
-label.style.color="hsl(0, 100%, 67%)";
- 
- if(day==''){
-    dayError.textContent= EmptyError;
-    return false;
-   }
-   else if(day<1 || day>31){
-    dayError.textContent= validDayError;
-    return false;
-   }
-   else{
-      dayError.textContent=" ";
-     dayInput.style.borderColor="hsl(0, 0%, 86%)";
-    label.style.color="hsl(0, 1%, 44%)";
-    return true;
-   }
-
-}
-
-function checkmonth(){
-const month =monthInput.value;
-const label =monthInput.previousElementSibling.previousElementSibling;
- monthInput.style.borderColor="hsl(0, 100%, 67%)";
-label.style.color="hsl(0, 100%, 67%)";
-   if(month==''){
-    monthError.textContent= EmptyError;
-    return false;
-   }
-   else if(month<1 || month>12){
-    monthError.textContent= validMonthError;
-    return false;
-   }
-   else{
-    monthError.textContent=" ";
-    monthInput.style.borderColor="hsl(0, 0%, 86%)";
-    label.style.color="hsl(0, 1%, 44%)";
-    return true;
-   }
-}
-
-
-function checkyear(){
-    const year =yearInput.value;
-    const label =yearInput.previousElementSibling.previousElementSibling;
-    yearInput.style.borderColor="hsl(0, 100%, 67%)";
-   label.style.color="hsl(0, 100%, 67%)";
-    const now = new Date().getFullYear();
-
-   if(year==""){
-    yearError.textContent= EmptyError;
-    return false;
-   }
-   else if(year>now){
-    yearError.textContent= futureYearError;
-  
-    return false;
-   }
-  
-
-   else{
-      yearError.textContent=" "
-      yearInput.style.borderColor="hsl(0, 0%, 86%)";
-      label.style.color="hsl(0, 1%, 44%)";
-     
-    return true;
-   }
+// Event listener to prevent users from entering special characters in the input fields
+form.addEventListener("keydown", function (e) {
+  if (invalidChars.includes(e.key)) {
+    e.preventDefault();
   }
+});
 
-  function animateNumberOutput(outputElement, targetValue) {
-   let currentNumber = 1;
-   const interval = setInterval(() => {
-     outputElement.textContent = currentNumber;
-     currentNumber++;
- 
-     if (currentNumber > targetValue) {
-       clearInterval(interval);
-       outputElement.textContent = targetValue;
-     }
-   }, 50); // Change the interval to control the animation speed (milliseconds)
- }
+// Function to limit the length of the input value
+function limitLength(event, value, limit) {
+  if (value.toString().length >= limit) {
+    event.preventDefault();
+  }
+}
+
+// Object containing validation conditions and error messages for each input field
+const validationdata = {
+  day: {
+    condition: day => day > 0 && day < 32,
+    errorMessage: "Must be a valid day",
+  },
+  month: {
+    condition: month => month > 0 && month < 13,
+    errorMessage: "Must be a valid month",
+  },
+  year: {
+    condition: year => {
+      const now = new Date().getFullYear();
+      return year < now;
+    },
+    errorMessage: "Must be in the past",
+  },
+};
+
+// Function to validate input values and display error messages
+function inputValidation(inputElement, errorElement, validationType) {
+  const value = inputElement.value;
+  const label = inputElement.previousElementSibling.previousElementSibling;
+  const validation = validationdata[validationType].condition(value);
+  inputElement.style.borderColor = validation ? "hsl(0, 0%, 86%)" : "hsl(0, 100%, 67%)";
+  label.style.color = validation ? "hsl(0, 1%, 44%)" : "hsl(0, 100%, 67%)";
+  errorElement.textContent = value ? (validation ? " " : validationdata[validationType].errorMessage) : "This field is required";
+  return validation;
+}
+
+// Function to animate the number output
+function animateNumberOutput(outputElement, targetValue) {
+  let currentNumber = 1;
+  const interval = setInterval(() => {
+    outputElement.textContent = currentNumber;
+    currentNumber++;
+
+    if (currentNumber > targetValue) {
+      clearInterval(interval);
+      outputElement.textContent = targetValue;
+    }
+  }, 50); // Change the interval to control the animation speed (milliseconds)
+}
+
+// Event listener for the calculate button
+btn.addEventListener("click", (e) => {
+  // Validate the day, month, and year inputs
+  const isValidDay = inputValidation(dayInput, dayError, "day");
+  const isValidMonth = inputValidation(monthInput, monthError, "month");
+  const isValidYear = inputValidation(yearInput, yearError, "year");
+
+  // If all inputs are valid, calculate the age
+  if (isValidDay && isValidMonth && isValidYear) {
+    const year = yearInput.value;
+    const day = dayInput.value;
+    const month = monthInput.value;
+
+    // Check if it's a leap year and update the number of days in February
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      monthDay[1] = 29;
+    } else {
+      monthDay[1] = 28;
+    }
+
+    // Check if the day is valid for the selected month
+    if (day > monthDay[month - 1]) {
+      const daylabel = dayInput.previousElementSibling.previousElementSibling;
+      dayInput.style.borderColor = "hsl(0, 100%, 67%)";
+      daylabel.style.color = "hsl(0, 100%, 67%)";
+      dayError.textContent = "Must be a valid date";
+    } else {
+      // If everything is valid, calculate the age
+      calculate(day, month, year);
+    }
+  }
+});
+// Function to calculate the age and animate the output
 function calculate(day,month,year){
    let date = new Date();
    let  currentYear =date.getFullYear();
@@ -145,28 +129,9 @@ function calculate(day,month,year){
       currentMonth =currentMonth+12;
       currentYear= currentYear-1;
 }
-
 animateNumberOutput(dayOutput, currentDate - day);
-  animateNumberOutput(monthOutput, currentMonth - month);
-  animateNumberOutput(yearOutput, currentYear - year);
+animateNumberOutput(monthOutput, currentMonth - month);
+animateNumberOutput(yearOutput, currentYear - year);
 }
-btn.addEventListener("click",(e)=>{
-   if(checkday()&&checkmonth()&&checkyear()){
-      const year =yearInput.value;
-      const day =dayInput.value;
-      const month =monthInput.value;
-      if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-         monthDay[1] = 29;
-      }
-       if(day>monthDay[month-1]){
-          allInput.style.borderColor ="hsl(0, 100%, 67%)";
-          allLabel.style.color="hsl(0, 1%, 44%)";
-          dayError.textContent="Must be a valid date"
-      }
-      else{
-         calculate(day,month,year);
-      }
-   }
-})
 
 
